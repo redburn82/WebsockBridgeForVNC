2069357fcadeb5a522ef36c37670f1c65f2152ca
redburn82 <redburn82@gmail.com>
2021-11-19 07:33:55 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 pkg install python2 python3 git -y
 pip install python-websockify
-mkdir -P .termux/tasker/
+mkdir -p .termux/tasker/
 cd .termux/tasker/
 git clone https://github.com/novnc/noVNC.git
 
