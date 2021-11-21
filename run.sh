ee6438ba3e7b3d26befbe81366df46a60ee32a7c
redburn82 <redburn82@gmail.com>
2021-11-21 14:14:19 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 pkg install -y python2 python3 git vim
 pip install -y python-websockify
-mkdir -p .termux/tasker/
-cd .termux/tasker/
 git clone https://github.com/novnc/noVNC.git
+termux-fix-shebang ~/noVNC/utils/websockify/run
+mkdir -p ~/.termux/tasker/
+echo "bash noVNC/utils/novnc_proxy --vnc 127.0.0.1:5901" > run.sh
 
